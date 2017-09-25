85366351f01a3b19a8c6750782f0f6be6b11d864
Eric Galdino <ericgaldino0@gmail.com>
2017-09-25 10:56:32 -0300

    Adicionando mais uma implementação do quickSort

--- src/br/eric/quicksort/QuickSort.java
+++ src/br/eric/quicksort/QuickSort.java
@@ -7,7 +7,8 @@ public class QuickSort {
 		int[] vetorQuickSort = { 8, 2, 10, 1, 5, 3, 20, 12, 4 };
 
 		System.out.println("Ordenado pelo QUICKSORT:\n");
-		quickSort(vetorQuickSort, 0, vetorQuickSort.length - 1);
+		//quickSort(vetorQuickSort, 0, vetorQuickSort.length - 1);
+		quickSortDois(vetorQuickSort, 0, vetorQuickSort.length - 1);
 
 		for (int i = 0; i < vetorQuickSort.length; i++) {
 			System.out.println(vetorQuickSort[i]);
@@ -15,7 +16,41 @@ public class QuickSort {
 
 	}
 	
-	// QuickSort Recursivo
+	//QuickSort DOIS
+	private static void quickSortDois(int[] v, int inicio, int fim) {
+		if(inicio < fim){
+			int posPivo = separar(v, inicio, fim);
+			quickSortDois(v, inicio, posPivo-1);
+			quickSortDois(v, posPivo+1, fim);
+		}
+	}
+
+	//Separa o vetor e retorna a posição do pivo
+	private static int separar(int[] v, int inicio, int fim) {
+		int pivo = v[inicio];
+		int esq = inicio+1;
+		int dir = fim;
+		int troca;
+		
+		while(esq <= dir){
+			if(v[esq] <= pivo)esq++;
+			else if(pivo < v[dir])dir--;
+			else{
+				troca = v[esq];
+				v[esq] = v[dir];
+				v[dir] = troca;
+				esq++;
+				dir--;
+			}
+		}
+		
+		v[inicio] = v[dir];
+		v[dir] = pivo;
+		
+		return dir;
+	}
+
+	// QuickSort UM
 	private static void quickSort(int[] v, int esquerda, int direita) {
 		int esq = esquerda;
 		int dir = direita;
